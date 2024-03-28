7ae3371a2dfaa578e3bedd4fadb1fb436b942c34
sayuru117 <sayuru117@gmail.com>
2024-03-28 16:23:27 +0530

    2nd commit

---
+++ src/com/sayuru/Test.java
@@ -0,0 +1,11 @@
+package com.sayuru;
+
+public class Test {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i !=1)
+            System.out.println(i);
+
+    }
+
+}
